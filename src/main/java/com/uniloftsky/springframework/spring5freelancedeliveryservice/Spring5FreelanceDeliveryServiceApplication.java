2323c4103462564d4f5d665a397b36de2d480078
uniloftsky <uniloftsky@gmail.com>
2021-04-08 23:05:59 +0300

    project initialized

---
+++ src/main/java/com/uniloftsky/springframework/spring5freelancedeliveryservice/Spring5FreelanceDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uniloftsky.springframework.spring5freelancedeliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5FreelanceDeliveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5FreelanceDeliveryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniloftsky/springframework/spring5freelancedeliveryservice/Spring5FreelanceDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniloftsky.springframework.spring5freelancedeliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5FreelanceDeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
