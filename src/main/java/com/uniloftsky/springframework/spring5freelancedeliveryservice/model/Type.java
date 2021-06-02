4fc149f74736387a20e188cb0a7ee8fa9284ef38
uniloftsky <uniloftsky@gmail.com>
2021-06-02 22:07:17 +0300

    changed name of type table

--- src/main/java/com/uniloftsky/springframework/spring5freelancedeliveryservice/model/Type.java
+++ src/main/java/com/uniloftsky/springframework/spring5freelancedeliveryservice/model/Type.java
@@ -5,11 +5,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "advertisementType")
 @Entity
 public class Type extends BaseEntity {
 
