09da9053c24b2cd7e013a5d363eed91921136fdc
uniloftsky <uniloftsky@gmail.com>
2021-06-04 20:22:10 +0300

    changed DetailsDTO and mapping for it

--- src/main/java/com/uniloftsky/springframework/spring5freelancedeliveryservice/api/v1/mappers/DetailsMapper.java
+++ src/main/java/com/uniloftsky/springframework/spring5freelancedeliveryservice/api/v1/mappers/DetailsMapper.java
@@ -2,13 +2,8 @@ package com.uniloftsky.springframework.spring5freelancedeliveryservice.api.v1.ma
 
 import com.uniloftsky.springframework.spring5freelancedeliveryservice.api.v1.model.DetailsDTO;
 import com.uniloftsky.springframework.spring5freelancedeliveryservice.model.Details;
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-@Mapper
 public interface DetailsMapper {
-
-    DetailsMapper INSTANCE = Mappers.getMapper(DetailsMapper.class);
 
     DetailsDTO detailsToDetailsDTO(Details details);
     Details detailsDTOToDetails(DetailsDTO detailsDTO);
