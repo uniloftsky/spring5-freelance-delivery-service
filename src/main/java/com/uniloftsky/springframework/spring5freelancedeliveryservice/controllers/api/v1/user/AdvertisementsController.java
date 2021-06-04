b673344003d28c53c69e8f149ef75e1d6ebdb05c
uniloftsky <uniloftsky@gmail.com>
2021-06-04 15:41:42 +0300

    documented AdvertisementsController

--- src/main/java/com/uniloftsky/springframework/spring5freelancedeliveryservice/controllers/api/v1/user/AdvertisementsController.java
+++ src/main/java/com/uniloftsky/springframework/spring5freelancedeliveryservice/controllers/api/v1/user/AdvertisementsController.java
@@ -5,6 +5,9 @@ import com.uniloftsky.springframework.spring5freelancedeliveryservice.api.v1.mod
 import com.uniloftsky.springframework.spring5freelancedeliveryservice.services.advertisement.AdvertisementService;
 import com.uniloftsky.springframework.spring5freelancedeliveryservice.services.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -32,6 +35,10 @@ public class AdvertisementsController extends AbstractController {
 
 
     @Operation(summary = "Get all advertisements what belongs to a user.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "401", description = "Auth needed. Provide authentication header with Bearer token."),
+            @ApiResponse(responseCode = "200", description = "Successful operation.")
+    })
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public Set<AdvertisementDTO> getUserAdvertisements(Authentication authentication) {
@@ -39,38 +46,63 @@ public class AdvertisementsController extends AbstractController {
     }
 
     @Operation(summary = "Get user's advertisement by ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "401", description = "Auth needed. Provide authentication header with Bearer token."),
+            @ApiResponse(responseCode = "200", description = "Successful operation."),
+            @ApiResponse(responseCode = "404", description = "User doesn't have advertisement with specified ID")
+    })
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
-    public AdvertisementDTO getUserAdvertisement(@PathVariable("id") Long id, Authentication authentication) {
+    public AdvertisementDTO getUserAdvertisement(@Parameter(description = "ID of advertisement what will be found") @PathVariable("id") Long id, Authentication authentication) {
         return advertisementMapper.advertisementToAdvertisementDTO(advertisementService.findUserAdvertisement(id, authentication.getName()));
     }
 
     @Operation(summary = "Delete user's advertisement by ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "401", description = "Auth needed. Provide authentication header with Bearer token."),
+            @ApiResponse(responseCode = "200", description = "Successful operation."),
+            @ApiResponse(responseCode = "404", description = "User doesn't have advertisement with specified ID")
+    })
     @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteUserAdvertisement(@PathVariable("id") Long id, Authentication authentication) {
+    public ResponseEntity<String> deleteUserAdvertisement(@Parameter(description = "Advertisement ID what will be deleted") @PathVariable("id") Long id, Authentication authentication) {
         advertisementService.delete(id, getUser(authentication));
         return new ResponseEntity<>("Advertisement deleted", new HttpHeaders(), HttpStatus.OK);
     }
 
     @Operation(summary = "Create user's advertisement.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "401", description = "Auth needed. Provide authentication header with Bearer token."),
+            @ApiResponse(responseCode = "200", description = "Successful operation.")
+    })
     @ResponseStatus(HttpStatus.OK)
     @PostMapping
-    public AdvertisementDTO createUserAdvertisement(@RequestBody AdvertisementDTO advertisement, Authentication authentication) {
+    public AdvertisementDTO createUserAdvertisement(@Parameter(description = "Advertisement what will be created") @RequestBody AdvertisementDTO advertisement, Authentication authentication) {
         return advertisementService.save(advertisementMapper.advertisementDTOToAdvertisement(advertisement), getUser(authentication));
     }
 
     @Operation(summary = "Patch user's advertisement by ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "401", description = "Auth needed. Provide authentication header with Bearer token."),
+            @ApiResponse(responseCode = "200", description = "Successful operation."),
+            @ApiResponse(responseCode = "404", description = "User doesn't have advertisement with specified ID")
+    })
     @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/{id}")
-    public AdvertisementDTO patchUserAdvertisement(@PathVariable("id") Long id, @RequestBody AdvertisementDTO advertisement, Authentication authentication) {
+    public AdvertisementDTO patchUserAdvertisement(@Parameter(description = "Advertisement ID what will be patched") @PathVariable("id") Long id, @Parameter(description = "Fields of advertisement to patch") @RequestBody AdvertisementDTO advertisement, Authentication authentication) {
         return advertisementService.patch(advertisement, getUser(authentication), id);
     }
 
     @Operation(summary = "Appoint driver to a specified user's advertisement.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "401", description = "Auth needed. Provide authentication header with Bearer token."),
+            @ApiResponse(responseCode = "200", description = "Successful operation."),
+            @ApiResponse(responseCode = "404", description = "User doesn't have advertisement with specified ID"),
+            @ApiResponse(responseCode = "400", description = "User can't execute advertisement what belongs to him.")
+    })
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "/appoint", params = {"advertisement_id", "driver_id"})
-    public AdvertisementDTO appointDriverToAdvertisement(@RequestParam("advertisement_id") Long advertisementId, @RequestParam("driver_id") Long driverId, Authentication authentication) {
+    public AdvertisementDTO appointDriverToAdvertisement(@Parameter(description = "Advertisement ID to which driver will be appointed") @RequestParam("advertisement_id") Long advertisementId, @Parameter(description = "ID of a driver who will be appointed to an advertisement") @RequestParam("driver_id") Long driverId, Authentication authentication) {
         return advertisementService.appointDriverToAdvertisement(advertisementId, driverId, authentication.getName());
     }
 
