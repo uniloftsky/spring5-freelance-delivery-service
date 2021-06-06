c892626b955505bf5d9594ba53908f7741e050ef
uniloftsky <uniloftsky@gmail.com>
2021-06-06 20:59:22 +0300

    changed dataloader behaviour for heroku profile

--- src/main/java/com/uniloftsky/springframework/spring5freelancedeliveryservice/bootstrap/DataLoader.java
+++ src/main/java/com/uniloftsky/springframework/spring5freelancedeliveryservice/bootstrap/DataLoader.java
@@ -13,6 +13,7 @@ import com.uniloftsky.springframework.spring5freelancedeliveryservice.services.d
 import com.uniloftsky.springframework.spring5freelancedeliveryservice.services.notification.NotificationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -44,6 +45,7 @@ public class DataLoader implements CommandLineRunner {
         this.userMapper = userMapper;
     }
 
+    @Profile("default")
     @Override
     public void run(String... args) throws InterruptedException {
 
